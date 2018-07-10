effc5f9998db0f9e887762eb9bee5590a4f09743
Alexander Marin <alexanderm2230@gmail.com>
2018-07-10 01:48:15 -0600

    Including docstrings and changing indexing of users object

--- lib/pyzk/pyzk.py
+++ lib/pyzk/pyzk.py
@@ -16,8 +16,10 @@ and ZKUser class, used to store user's data.
 Author: Alexander Marin <alexanderm2230@gmail.com>
 """
 
-
 class ZKUser:
+    """
+    Class to model user's properties.
+    """
     def __init__(self):
         self.user_sn = None         # user's internal index
         self.user_id = ''           # user's id
@@ -26,7 +28,9 @@ class ZKUser:
         self.card_number = None     # user's RF card number
         self.admin_level = 0        # user's admin level
         self.not_enabled = 1        # user's enable flag(active=0)
-        self.user_fptmps = [[0,0]]*10   # user's fingerprint templates
+        # user's fingerprint templates
+        # with the format [fp template, fp flag]
+        self.user_fptmps = [[0,0]]*10
 
     def set_user_info(self, user_sn, user_id, name, password,
                       card_no, admin_lv, neg_enabled):
@@ -39,7 +43,8 @@ class ZKUser:
         :param password: String, user password, 8 chars long.
         :param card_no: Integer, user RF card number.
         :param admin_lv: Integer, user admin level.
-        :param neg_enabled: Integer, user enable flag, valid values {0,1}.
+        :param neg_enabled: Integer, user enable flag,
+        (0=enabled, 1=disabled).
         :return:None.
         """
         self.user_sn = user_sn
@@ -49,6 +54,14 @@ class ZKUser:
         self.card_number = card_no
         self.admin_level = admin_lv
         self.not_enabled = neg_enabled
+
+    def get_sn(self):
+        """
+        Returns the user's internal index on machine.
+
+        :return: Integer.
+        """
+        return self.user_sn
 
     def set_user_fptmp(self, fp_index, fp_tmp, fp_flag):
         """
@@ -62,36 +75,82 @@ class ZKUser:
         self.user_fptmps[fp_index] = [fp_tmp, fp_flag]
 
 
+class ATTen:
+    """
+    Attendance log entry.
+    """
+    def __init__(self, user_sn, user_id, ver_type, att_time, ver_state):
+        """
+        :param user_sn: Integer, user's index on machine.
+        :param user_id: Str, user's ID.
+        :param ver_type: Integer, verification type of attendance.
+        :param att_time: Datetime object, time of the record.
+        :param ver_state: Integer, verification state.
+        """
+        self.user_sn = user_sn
+        self.user_id = user_id
+        self.ver_type = ver_type
+        self.att_time = att_time
+        self.ver_state = ver_state
+
+
+class OPen:
+    """
+    Operation log entry.
+    """
+    def __init__(self, op_id, op_time, param1, param2, param3, param4):
+        """
+        :param op_id: Integer, operation code.
+        :param op_time: Datetime object, time of the record.
+        :param param1: Parameter 1.
+        :param param2: Parameter 2.
+        :param param3: Parameter 3.
+        :param param4: Parameter 4.
+        """
+        self.op_id = op_id
+        self.op_time = op_time
+        self.param1 = param1
+        self.param2 = param2
+        self.param3 = param3
+        self.param4 = param4
+
+
 class ZKSS(PacketMixin, DataUserMixin,
            DataRecordMixin, TerminalMixin,
            AccessMixin, RealtimeMixin, OtherMixin):
 
     def __init__(self):
-        self.reply_number = 0
-        self.session_id = 0
-        self.connected_flg = False
-        self.dev_platform = ''
-        self.firmware_v = ''
-        self.users = {}
+        self.reply_number = 0           # reply counter
+        self.session_id = 0             # session id
+        self.connected_flg = False      # connection flag
+        self.dev_platform = ''          # platform name
+        self.firmware_v = ''            # firmware version
+        self.users = {}                 # dict of ZKUser, the key is the id
+        self.att_log = []               # list of attendance entries
+        self.op_log = []                # list of operation entries
 
-    def add_user(self, user_id):
+    def add_user(self, user_sn):
         """
-        Appends an empty user instance, given the user id,
+        Appends an empty user instance, given the user index,
         to the list of users of the current session.
 
-        :param user_id: String, user ID.
+        :param user_sn: Integer, user's index on machine.
         :return: None.
         """
-        self.users[user_id] = ZKUser()
+        self.users[user_sn] = ZKUser()
 
-    def print_users_summary(self):
+    def print_users_summary(self, users_sns=None):
         """
-        Prints a tables with relevant users data,
+        Prints a table with relevant users data and another with the
+        templates with the corresponding owners,
         read_all_user_id and read_all_fptmp should be called before, since
-        this function only prints the contents of the users list object.
+        this function only prints the contents of the ZKUser list object.
 
         :return: None.
         """
+        if users_sns == None:
+            users_sns = self.users.keys()
+
         # create users info table
         # fields to show
         t_headers = ['User internal index', 'User ID', 'User name',
@@ -100,11 +159,11 @@ class ZKSS(PacketMixin, DataUserMixin,
         summ_table = PrettyTable(t_headers)
 
         # adds a row for each user
-        for uid in self.users:
-            zuser = self.users[uid]
+        for sn in users_sns:
+            zuser = self.users[sn]
             summ_table.add_row([
-                                str(zuser.user_sn),
-                                uid,
+                                sn,
+                                zuser.user_id,
                                 zuser.user_name,
                                 zuser.user_password,
                                 zuser.card_number,
@@ -123,8 +182,9 @@ class ZKSS(PacketMixin, DataUserMixin,
         fptmp_table = PrettyTable(t_headers)
 
         # adds a row for each template
-        for uid in self.users:
-            zuser = self.users[uid]
+        for sn in users_sns:
+
+            zuser = self.users[sn]
 
             for fp_idx in range(len(zuser.user_fptmps)):
                 # if fp template doesn't exists, skip
@@ -135,7 +195,7 @@ class ZKSS(PacketMixin, DataUserMixin,
                 fptmp = zuser.user_fptmps[fp_idx][0]
                 flg = zuser.user_fptmps[fp_idx][1]
                 fptmp_table.add_row([
-                    uid,
+                    zuser.user_id,
                     zuser.user_name,
                     fp_idx,
                     flg,
@@ -144,15 +204,94 @@ class ZKSS(PacketMixin, DataUserMixin,
 
         print(fptmp_table)
 
-    def sn_to_id(self, sn):
+    def print_attlog(self):
+        """
+        Prints a table with attendance log entries, the function read_att_log
+        should be called before calling this function.
+
+        :return: None.
+        """
+        t_headers = ['User internal index', 'User ID', 'Verification type',
+                     'Verification state', 'Attendance time']
+
+        att_table = PrettyTable(t_headers)
+
+        for att_entry in self.att_log:
+            att_table.add_row([
+                att_entry.user_sn,
+                att_entry.user_id,
+                att_entry.ver_type,
+                att_entry.ver_state,
+                att_entry.att_time
+            ])
+        # show table
+        print(att_table)
+
+    def print_oplog(self):
+        """
+        Prints a table with the operation log entries,
+        the function read_op_log, should be called before calling this
+        function.
+
+        :return: None.
+        """
+        t_headers = ['Operation ID', 'Operation time', 'Param 1',
+                     'Param 2', 'Param 3', 'Param 4']
+
+        op_table = PrettyTable(t_headers)
+
+        for op_entry in self.op_log:
+            op_table.add_row([
+                op_entry.op_id,
+                op_entry.op_time,
+                op_entry.param1,
+                op_entry.param2,
+                op_entry.param3,
+                op_entry.param4
+            ])
+        # show table
+        print(op_table)
+
+    def id_to_sn(self, user_id):
         """
         Obtains the user id, given the user internal index.
 
-        :param sn: Integer, user's internal index on machine.
-        :return: String, user ID, if the user doesn't exists, returns an
-        empty string.
+        :param user_id: Str, user's ID.
+        :return: Integer, user's index on machine,
+        if the user doesn't exists, returns -1.
         """
-        for uid in list(self.users):
-            if self.users[uid].user_sn == sn:
-                return uid
-        return ''
+        for sn in list(self.users):
+            if self.users[sn].user_id == user_id:
+                return user_id
+        return -1
+
+    def append_att_entry(self, user_sn, user_id, ver_type,
+                         att_time, ver_state):
+        """
+        Appends an attendance log entry to the attendance object.
+
+        :param user_sn: Integer, user's index.
+        :param user_id: Str, user's ID.
+        :param ver_type: Integer, verification type.
+        :param att_time: Datetime, time of the record.
+        :param ver_state: Integer, verification state.
+        :return: None.
+        """
+        self.att_log += [ATTen(user_sn, user_id, ver_type,
+                               att_time, ver_state)]
+
+    def append_op_entry(self, op_id, op_time, param1,
+                        param2, param3, param4):
+        """
+        Appends an operation log entry to the operation records object.
+
+        :param op_id: Integer, operation code.
+        :param op_time: Datetime, time of the record.
+        :param param1: Parameter 1.
+        :param param2: Parameter 2.
+        :param param3: Parameter 3.
+        :param param4: Parameter 4.
+        :return: None.
+        """
+        self.op_log += [OPen(op_id, op_time, param1,
+                             param2, param3, param4)]
