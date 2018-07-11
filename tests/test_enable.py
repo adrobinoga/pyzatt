fd302a039b7de9e2b8769c5b5aa9d64314b5043c
Alexander Marin <alexanderm2230@gmail.com>
2018-07-11 17:11:51 -0600

    Adding simple script to enable device

---
+++ tests/test_enable.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.5
+import time
+import os.path
+from utils import *
+import pyzk.pyzk as pyzk
+from pyzk.zkmodules.defs import *
+
+"""
+Test script to enable the machine
+
+WARNING: Apply this test to devices that aren't under current use,
+    if a deployed device is used, remember to upload the data to
+    the device(Sync) using the ZKAccess software, that will
+    overwrite any changes made by the script.
+
+Author: Alexander Marin <alexanderm2230@gmail.com>
+"""
+
+time.sleep(0)
+
+ip_address = '192.168.19.152'  # set the ip address of the device to test
+machine_port = 4370
+
+z = pyzk.ZKSS()
+z.connect_net(ip_address, machine_port)
+z.disable_device()
+
+print_header("Eneabling device")
+
+z.enable_device()
+z.disconnect()
