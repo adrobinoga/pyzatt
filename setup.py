7f0924a50b2039c0a6594cbac4f4d1cfe859b2bd
Alexander Marin <alexanderm2230@gmail.com>
2018-11-13 23:11:31 -0600

    Adding dependencies and long description to setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='PyZatt',
     version='1.0',
     description='Python lib to manage ZKTeco attendance devices',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Alexander Marin',
     author_email='alexanderm2230@gmail.com',
     url='https://github.com/adrobinoga/pyzatt',
     packages=find_packages('lib'),
-    package_dir={'':'lib'}
+    package_dir={'': 'lib'},
+    install_requires=[
+          'prettytable', 'colorama',
+    ]
     )
