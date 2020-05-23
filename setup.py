dc30714ed641388f53537319f6c0e7bd8dba544a
Alexander Marin <alexanderm2230@gmail.com>
2020-05-23 14:38:28 -0600

    Revert "Bump version: 2.0.0 → 2.0.0"
    This reverts commit 6e81779e458d28650d39e9837781aeaff9bda2fd.
    
    Resolves: #17

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/adrobinoga/pyzatt',
-    version='version='2.0.0'',
+    version='2.0.0',
     zip_safe=False,
 )
